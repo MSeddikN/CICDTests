3ec4ce55cb3ef392247bc427813c2a8a43ca833e
MSeddikN <mseddik@noon.com>
2024-11-16 15:16:15 +0200

    Test 20

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 package src;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi World");
+//    public static void main(String[] args) {
+//        System.out.println("Hi World");
+//    }
+    public static String sayHello(String name) {
+        // You can print to STDOUT for debugging if you need to:
+        System.out.print("Hello "+name);
+
+        // but you need to return the correct value in order to pass the challenge
+        return name; // TODO: return the correct value
+    }
+    public static void main(String[]args){
+//        firstNonRepeatingLetter("sTreSs");
+        System.out.println(firstNonRepeatingLetter("moonmen"));
+    }
+
+
+    public static String firstNonRepeatingLetter(String str) {
+        String strLowerCase = str.toLowerCase();
+        for(int i =0; i<str.length();i++){
+            var charOriginal = str.charAt(i);
+            var charLower = strLowerCase.charAt(i);
+//            System.out.println(strLowerCase.lastIndexOf(charLower));
+//            System.out.println(charLower);
+            if (strLowerCase.lastIndexOf(charLower)==i && strLowerCase.indexOf(charLower) == i) {
+
+                return String.valueOf(charOriginal); // Return the original case
+            }
+        }
+        return "";
     }
 }
