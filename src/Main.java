53cccf04a5ed5dd4b2259d5480f8f312e3717939
MSeddikN <mseddik@noon.com>
2024-11-21 21:37:39 +0200

    Test 2 CICD

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 //        firstNonRepeatingLetter("sTreSs");
 //        System.out.println(firstNonRepeatingLetter("moonmen"));
         reverseNumber(98);
-        palindromeChainLength(12);
+        palindromeChainLength(121);
     }
 
 
