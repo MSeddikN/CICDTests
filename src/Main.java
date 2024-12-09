f6cf23f59cc4ffeb91e89d2e7f05faf191f3fde8
MSeddikN <mseddik@noon.com>
2024-12-09 12:29:06 +0200

    CICD 5

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[]args){
 //        firstNonRepeatingLetter("sTreSs");
 //        System.out.println(firstNonRepeatingLetter("moonmen"));
-        reverseNumber(981);
+        reverseNumber(98);
         palindromeChainLength(12);
     }
 
